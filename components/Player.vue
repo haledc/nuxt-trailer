8b3493adc9cf2738f1e7552fab99317f6600df74
Hale Deng <haledeng88@gmail.com>
2019-11-07 17:24:51 +0800

    feat: add Player component

---
+++ components/Player.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="player">
+    <div
+      v-video-player:VueVideoPlayer="{
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [{ type: 'video/mp4', src: movieDetail.url }],
+        poster: movieDetail.pic,
+        height: '800',
+        width: '1400'
+      }"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movieDetail: { type: Object, default: () => {} }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.player {
+  max-width: 1400px;
+  max-height: 800px;
+}
+</style>
